daa177b257b38a5e1f0b2c7687cb6f61b54f3da1
unknown <rodrigo.ulloa@latam.com>
2020-09-01 00:18:41 -0400

    03 se modifica unos errores de configuracion y se crea  Repositorio y Convertidor

---
+++ src/main/java/com/restfull/example/repository/NotaRepository.java
@@ -0,0 +1,19 @@
+package com.restfull.example.repository;
+
+import java.io.Serializable;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.restfull.example.entity.Nota;
+
+@Repository
+public interface NotaRepository extends JpaRepository<Nota, Serializable> {
+
+	public abstract Nota findByNombre(String nombre);
+
+	public abstract List<Nota> findByTitulo(String titulo);
+
+	public abstract Nota findByNombreAndTitulo(String nombre, String titulo);
+}
