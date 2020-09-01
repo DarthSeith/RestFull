69705dafa2a0a439dfee93402b89b2330fe221df
unknown <rodrigo.ulloa@latam.com>
2020-08-31 23:21:36 -0400

    01 Crear una API RESTFull con Spring y Hibernate Configuración https://www.youtube.com/watch?v=UpdkMjunXSk&list=PLcIHm18h1i4nD4H8tPeID8PNiKsm4VZm5&index=56

---
+++ src/main/java/com/restfull/example/MicroservicioRestfullV1Application.java
@@ -0,0 +1,13 @@
+package com.restfull.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioRestfullV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioRestfullV1Application.class, args);
+	}
+
+}
